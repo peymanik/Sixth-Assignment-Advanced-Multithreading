1a57687fa0f97094e50912e3b9ac3889f0ba52de
unknown <peymanick@gmail.com>
2024-05-22 15:48:42 +0330

    SemaphoreDone

--- src/main/java/sbu/cs/Report.md
+++ src/main/java/sbu/cs/Report.md
@@ -11,3 +11,7 @@ The second algorithm was the [Spigot Algorithm of Rabinowitz and Wagon](https://
 ## Chudnovsky Algorithm
 
 The last algorithm was the [Chudnovsky algorithm](https://en.wikipedia.org/wiki/Chudnovsky_algorithm#:~:text=The%20Chudnovsky%20algorithm%20is%20a,on%20Ramanujan's%20%CF%80%20formulae), which is considered the fastest and most optimized algorithm that exists. It was able to pass all tests by calculating only 80 elements of its series. You can find more info about the algorithm [here](https://observablehq.com/@galopin/the-chudnovsky-algorithm-for-calculating-pi).
+
+## Program structure
+Calculation of algorithms is divided into 4 part, for example in Chudnovsky algorithm, each part calculates 20 loops of series. Then a thread-pool will run each part in a separate threat to increase speed. 
+The Part class implements Runnable and inside that calculation is done. Inside calculate method, threads will be created.  
--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -1,6 +1,11 @@
 package sbu.cs.Semaphore;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Semaphore;
+
 public class Operator extends Thread {
+    Semaphore sem = new Semaphore(2);
 
     public Operator(String name) {
         super(name);
@@ -8,6 +13,16 @@ public class Operator extends Thread {
 
     @Override
     public void run() {
+        try {
+            sem.acquire();
+
+            LocalDateTime currentTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedCurrentTime = currentTime.format(formatter);
+            System.out.println(Thread.currentThread().getName()+"  "+formattedCurrentTime);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         for (int i = 0; i < 10; i++)
         {
             Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
@@ -17,5 +32,6 @@ public class Operator extends Thread {
                 e.printStackTrace();
             }
         }
+        sem.release();
     }
 }
